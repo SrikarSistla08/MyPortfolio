d56b538ea5a36b9967428df69755bb26891e9f9a
Srikar <73938704+SrikarSistla08@users.noreply.github.com>
2025-02-13 19:15:37 -0500

    pushitalic;

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,24 @@ import React from 'react';
 function Header() {
     return (
         <header className="flex justify-between items-center">
-            <h1 className="text-7xl font-bold">Srikar Sistla</h1>
+            <h1 className="text-7xl font-bold old-standard-tt-regular-italic hover:text-stone-500 transition-colors duration-200">Srikar Sistla</h1>
             <nav>
                 <ul className="flex gap-4">
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#experience">Experience</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <div className="hover:text-stone-500 transition-colors duration-200">
+                        <li><a href="#about">About</a></li>
+                    </div>
+                    <div className="hover:text-stone-500 transition-colors duration-200">
+                        <li><a href="#experience">Experience</a></li>
+                    </div>
+                    <div className="hover:text-stone-500 transition-colors duration-200">
+                        <li><a href="#skills">Skills</a></li>
+                    </div>
+                    <div className="hover:text-stone-500 transition-colors duration-200">
+                        <li><a href="#contact">Contact</a></li>
+                    </div>
+                    <div className="hover:text-stone-500 transition-colors duration-200">
+                        <li><a href="@components/photography">Photography</a></li>
+                    </div>
                 </ul>
             </nav>
         </header>
