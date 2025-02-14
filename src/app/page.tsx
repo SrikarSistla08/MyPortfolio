f4ebfb0f4819c995d4ecb79b9c27a929ebbc15ab
Srikar <73938704+SrikarSistla08@users.noreply.github.com>
2025-02-14 14:44:25 -0500

    Chanegs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,8 @@ export default function Home() {
         {/* Education Section */}
         <Education />
 
-        {/* Contact Section */}
-        <Contact />
-
         {/* My Creations Section */}
-        <section id="creations" className="flex flex-col gap-8 font-old-standard-tt-bold container">
+        <section id="creations" className="flex flex-col gap-8 font-[family-name:var(--font-geist-sans)]">
           <div className="flex items-center gap-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
@@ -47,6 +44,8 @@ export default function Home() {
             </Link>
           </div>
         </section>
+        {/* Contact Section */}
+        <Contact />
       </main>
 
       <footer className="text-center text-sm">
