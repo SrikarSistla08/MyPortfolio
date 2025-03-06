ed5085eb3ce7d4026c2e5b3bc5adc006dd53f034
Srikar <73938704+SrikarSistla08@users.noreply.github.com>
2025-03-06 12:54:02 -0500

    changes for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-slate-800 p-20 text-center text-sm flex items-center justify-center gap-2 tracking-widest font-['rajdhani']">
+    <footer className="bg-slate-800 text-white   p-20 text-center text-sm flex items-center justify-center gap-2 tracking-widest font-['rajdhani']">
         © {new Date().getFullYear()} Srikar Sistla. All rights reserved. Hire me LOL
     </footer>
     );
