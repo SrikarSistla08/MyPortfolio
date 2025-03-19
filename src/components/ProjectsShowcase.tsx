a518deff224bdfbb6c6eb88da3408148fe6536df
Srikar <73938704+SrikarSistla08@users.noreply.github.com>
2025-03-19 19:09:04 -0400

    Added  changes for projects

--- src/components/ProjectsShowcase.tsx
+++ src/components/ProjectsShowcase.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import Link from 'next/link';
 import { projectsData } from './Projects/projects';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
 const ProjectsShowcase: React.FC = () => {
   const featuredProjects = projectsData.slice(0, 3); // Display only the first 3 projects
 
+  // const getProjectIcon = (index: number) => {
+  //   switch (index) {
+  //     case 0:
+  //       return faCode;
+  //     case 1:
+  //       return faLaptopCode;
+  //     case 2:
+  //       return faMobileAlt;
+  //     default:
+  //       return faCode;
+  //   }
+  // };
+
   return (
     <section className="mb-12">
-      <h2 className="text-3xl mb-6">Featured Projects</h2>
+      <div className="flex items-center mb-6">
+        <FontAwesomeIcon icon={faCode} className="text-blue-500 text-3xl mr-3" />
+        <h2 className="text-3xl">Featured Projects</h2>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {featuredProjects.map((project, index) => (
-          <div key={index} className="bg-zinc-800 rounded shadow-md overflow-hidden">
+          <div key={index} className="bg-zinc-800 rounded shadow-md overflow-hidden rounded-lg shadow-lg hover:shadow-xl hover:shadow-blue-800/30 transition duration-300">
             <div className="p-6">
-              <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+              <div className="flex items-center mb-4">
+                <h3 className="text-xl font-semibold">{project.title}</h3>
+              </div>
               <p className="text-white-600 mb-4">{project.description}</p>
               <a
                 href={project.link}
@@ -36,3 +56,4 @@ const ProjectsShowcase: React.FC = () => {
 };
 
 export default ProjectsShowcase;
+
